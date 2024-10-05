22164990fb87d88d849aad3ac4e2224ca4cffba5
breduarm <bryan.e.armijos@gmail.com>
2024-10-05 14:00:00 -0500

    fix: Use FragmentContainerView instead of fragment tag

--- app/src/main/java/com/beam/friendlyeats/ui/home/MainActivity.kt
+++ app/src/main/java/com/beam/friendlyeats/ui/home/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
