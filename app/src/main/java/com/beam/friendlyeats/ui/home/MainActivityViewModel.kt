04a1b04906b7b6196569e3376f3e44399c87f587
breduarm <bryan.e.armijos@gmail.com>
2024-10-08 12:00:00 -0500

    feat(home): Create MainActivityViewModel

---
+++ app/src/main/java/com/beam/friendlyeats/ui/home/MainActivityViewModel.kt
@@ -0,0 +1,8 @@
+package com.beam.friendlyeats.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+
+    var isSigningIn: Boolean = false
+}
--- app/src/main/java/com/beam/friendlyeats/ui/home/MainFragment.kt
+++ app/src/main/java/com/beam/friendlyeats/ui/home/MainFragment.kt
@@ -1,18 +1,24 @@
 package com.beam.friendlyeats.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.beam.friendlyeats.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import com.beam.friendlyeats.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+    private val viewModel: MainActivityViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false);
+        return binding.root;
     }
 }
