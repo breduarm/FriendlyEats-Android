f199eb3be5afe10065948fa6ac3da87ef96b7d82
breduarm <bryan.e.armijos@gmail.com>
2024-10-08 11:00:00 -0500

    chore: Enable viewBinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,10 +26,16 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
