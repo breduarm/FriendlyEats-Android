3ca9cdeebb3f80630fdbe75be68c0a741e3a2606
breduarm <bryan.e.armijos@gmail.com>
2024-10-09 12:00:00 -0500

    feat(signIn): Let user sign in using firebase Auth API

--- app/src/main/java/com/beam/friendlyeats/ui/home/MainFragment.kt
+++ app/src/main/java/com/beam/friendlyeats/ui/home/MainFragment.kt
@@ -1,25 +1,36 @@
 package com.beam.friendlyeats.ui.home
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.ViewModelProvider
+import com.beam.friendlyeats.R
 import com.beam.friendlyeats.databinding.FragmentMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 
 class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
     private val viewModel: MainActivityViewModel by activityViewModels()
 
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { result -> this.onSignInResult(result) }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentMainBinding.inflate(inflater, container, false);
-        return binding.root;
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onStart() {
@@ -32,9 +43,48 @@ class MainFragment : Fragment() {
         }
     }
 
+    private fun shouldStartSignIn(): Boolean = !viewModel.isSigningIn
+
     private fun startSignIn() {
-        // TODO Start sign with firebase in when user is not signed in
+        val intent = AuthUI.getInstance().createSignInIntentBuilder()
+            .setAvailableProviders(listOf(AuthUI.IdpConfig.EmailBuilder().build()))
+            .setIsSmartLockEnabled(false)
+            .build()
+
+        signInLauncher.launch(intent)
+        viewModel.isSigningIn = true
     }
 
-    private fun shouldStartSignIn(): Boolean = !viewModel.isSigningIn
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        viewModel.isSigningIn = false
+
+        if (result.resultCode != Activity.RESULT_OK) {
+            when {
+                // User pressed the back button.
+                response == null -> {
+                    requireActivity().finish()
+                }
+                // A network error occurred.
+                response.error != null && response.error!!.errorCode == ErrorCodes.NO_NETWORK -> {
+                    showSignInErrorDialog(R.string.message_no_network)
+                }
+                // Unknown error.
+                else -> {
+                    showSignInErrorDialog(R.string.message_unknown)
+                }
+            }
+        }
+    }
+
+    private fun showSignInErrorDialog(@StringRes message: Int) {
+        val dialog = AlertDialog.Builder(requireContext())
+            .setTitle(R.string.title_sign_in_error)
+            .setMessage(message)
+            .setCancelable(false)
+            .setPositiveButton(R.string.option_retry) { _, _ -> startSignIn() }
+            .setNegativeButton(R.string.option_exit) { _, _ -> requireActivity().finish() }.create()
+
+        dialog.show()
+    }
 }
