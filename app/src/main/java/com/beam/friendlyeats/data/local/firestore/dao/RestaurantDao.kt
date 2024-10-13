d2b14998a4ed6278afa5477297f4f50818d110a1
breduarm <bryan.e.armijos@gmail.com>
2024-10-13 12:00:00 -0500

    feat(home): Config firestore to use local cached data

--- app/src/main/java/com/beam/friendlyeats/data/local/firestore/dao/RestaurantDao.kt
+++ app/src/main/java/com/beam/friendlyeats/data/local/firestore/dao/RestaurantDao.kt
@@ -3,7 +3,11 @@ package com.beam.friendlyeats.data.local.firestore.dao
 import com.beam.friendlyeats.data.local.firestore.collections.RestaurantCollection
 import com.beam.friendlyeats.data.local.firestore.mappers.toDomain
 import com.beam.friendlyeats.domain.models.Restaurant
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Source
+import com.google.firebase.firestore.firestoreSettings
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.persistentCacheSettings
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.tasks.await
 
@@ -14,12 +18,22 @@ interface RestaurantDao {
 
 class RestaurantDaoFirebaseImpl : RestaurantDao {
 
-    private val db = Firebase.firestore
+    private val db = initFirestore()
     private val collection = db.collection(RestaurantCollection.COLLECTION_KEY)
 
     override suspend fun findAllRestaurants(): List<Restaurant> = collection
-        .get()
+        .get(Source.CACHE)
         .await()
         .toObjects(RestaurantCollection::class.java)
         .map { it.toDomain() }
+
+    private fun initFirestore(): FirebaseFirestore {
+        val firestore = Firebase.firestore
+        val settings = firestoreSettings {
+            // Use persistent disk cache (default)
+            setLocalCacheSettings(persistentCacheSettings {})
+        }
+        firestore.firestoreSettings = settings
+        return firestore
+    }
 }
