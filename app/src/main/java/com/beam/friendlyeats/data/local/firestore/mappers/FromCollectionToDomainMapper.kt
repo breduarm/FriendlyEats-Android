97a971ee017ce0a8e3ea52533ad241ef47e3736c
breduarm <bryan.e.armijos@gmail.com>
2024-10-26 12:00:00 -0500

    feat(detail): Refactor mappers to unify in one file

--- app/src/main/java/com/beam/friendlyeats/data/local/firestore/mappers/FromCollectionToDomainMapper.kt
+++ app/src/main/java/com/beam/friendlyeats/data/local/firestore/mappers/FromCollectionToDomainMapper.kt
@@ -2,8 +2,16 @@ package com.beam.friendlyeats.data.local.firestore.mappers
 
 import com.beam.friendlyeats.data.local.firestore.collections.RatingCollection
 import com.beam.friendlyeats.data.local.firestore.collections.RestaurantCollection
+import com.beam.friendlyeats.data.local.firestore.collections.RestaurantsGroupCollection
 import com.beam.friendlyeats.domain.models.Rating
 import com.beam.friendlyeats.domain.models.Restaurant
+import com.beam.friendlyeats.domain.models.RestaurantsGroup
+
+fun RestaurantsGroupCollection.toDomain() = RestaurantsGroup(
+    groupName = groupName.orEmpty(),
+    restaurantsIds = restaurants.orEmpty(),
+    restaurants = emptyList(),
+)
 
 fun RestaurantCollection.toDomain() = Restaurant(
     name = name ?: "",
